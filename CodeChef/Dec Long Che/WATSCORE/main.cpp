09bfefc2d62b681548aae4b3be59861cce4e6e98
Sourav Kar <karsourav05@gmail.com>
2019-12-12 00:04:17 +0530

    Started Codechef

--- CodeChef/Dec Long Che/WATSCORE/main.cpp
+++ CodeChef/Dec Long Che/WATSCORE/main.cpp
@@ -1,4 +1,4 @@
-#include<std/c++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -11,13 +11,27 @@ int main() {
 	int t;
 	cin>>t;
 
-	int arr[1001][1001];
+	int Q[1001];
+	int S[1001];
+	int qn[] = {1,2,3,4,5,6,7,8};
+	int p;
 	while(t--) {
 		int n;
 		cin>>n;
+		for(int i=0; i<n; i++) {
+			cin>>p;
+			if(p<9) {
+				Q[i]=p;
+				cin>>S[i];
+			} else {
+				i++;
+			}
+		}
+		for(int i=0; i<n; i++) {
+			cout<<Q[i]<<" ";
+			cout<<S[i]<<endl;
+		}
 	}
-
-
 
 
 	return 0;
