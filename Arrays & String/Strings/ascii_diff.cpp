5567f85a389e8122d9bf71c85b91397137b36a9d
Sourav Kar <karsourav05@gmail.com>
2020-01-08 20:15:01 +0530

    Strings: ascii diff

--- Arrays & String/Strings/ascii_diff.cpp
+++ Arrays & String/Strings/ascii_diff.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	string s;
+	cin>>s;
+	int len = s.length();
+
+	int j=1;
+	for(int i=0; i<len-1; i++) {
+		cout<<s[i]<<(int)s[j]-(int)s[i];
+		j++;
+	}
+	cout<<s[len-1];
+
+
+	return 0;
+}
