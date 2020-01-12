1bd0e8254f12f993c7630032a9e65bc4e74f9ab9
Sourav Kar <karsourav05@gmail.com>
2020-01-12 22:23:44 +0530

    isPalindrome Recursive

--- Arrays & String/Strings/is_Palindrome.cpp
+++ Arrays & String/Strings/is_Palindrome.cpp
@@ -1,39 +1,52 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-bool isPalindrome(int *a, int s, int e) {
+bool isPalindrome(int *a, int size, int n) {
 
-	if(s>e) {
+	if(n==size/2) {
 		return true;
 	}
 
-	if(a[s]==a[e]) {
-		isPalindrome(a,++s,--e);
+	if(a[n-1]!=a[size-n]) {
+		return false;
 	}
 
-	return false;
+	isPalindrome(a,size,n+1);
 }
 
 int main() {
 
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdin);
+	freopen("output.txt", "w", stdout);
 	#endif
 
-	int n;
-	cin>>n;
-	int a[n];
-	for(int i=0; i<n; i++)
+	int size;
+	cin>>size;
+	int a[size];
+	for(int i=0; i<size; i++)
 		cin>>a[i];
 
-	// if(isPalindrome(a,0,n-1)) {
-	// 	cout<<"true";
-	// } else {
-	// 	cout<<"false";
-	// }
-	cout<<isPalindrome(a,0,n-1);
+	int x = isPalindrome(a,size,1);
+	if(x) cout<<"true";
+	else cout<<"false";
+
+	// isPalindrome(a,size,1);
 
 	return 0;
 }
+
+//input
+// 9
+// 5 3 0 0 7 0 0 0 0 
+// 6 0 0 1 9 5 0 0 0 
+// 0 9 8 0 0 0 0 6 0 
+// 8 0 0 0 6 0 0 0 3 
+// 4 0 0 8 0 3 0 0 1 
+// 7 0 0 0 2 0 0 0 6 
+// 0 6 0 0 0 0 2 8 0 
+// 0 0 0 4 1 9 0 0 5 
+// 0 0 0 0 8 0 0 7 9
+
+//output
+//false
