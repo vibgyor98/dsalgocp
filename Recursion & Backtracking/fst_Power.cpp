4b9b0940acb6f741df8c17f828cc3ea38f55a311
Sourav Kar <karsourav05@gmail.com>
2020-01-14 23:25:54 +0530

    fstPower solved

--- Recursion & Backtracking/fst_Power.cpp
+++ Recursion & Backtracking/fst_Power.cpp
@@ -1,1 +1,25 @@
-#include 
+#include <iostream>
+using namespace std;
+
+int fstPow(int a, int b) {
+
+	if(b==0) {
+		return 1;
+	} else {
+		return a*fstPow(a, b-1);
+	}
+}
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int a,b;
+	cin>>a>>b;
+	cout<<fstPow(a,b);
+
+	return 0;
+}
