52b8a5191db6c57aa22e8c7a5baba15fb7a65c74
Sourav Kar <karsourav05@gmail.com>
2020-01-19 15:03:35 +0530

    isSorted?

---
+++ Recursion & Backtracking/isSorted.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void isSorted(int *a, int n) {
+
+	//base case
+	if(n==0){
+		cout<<sorted;
+	}
+
+	//rec case
+	if(a[0]<)
+}
+
+int main() {
+
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+
+	isSorted(a,n);
+	cout<<endl;
+	cout<<is_sorted(a);
+}
