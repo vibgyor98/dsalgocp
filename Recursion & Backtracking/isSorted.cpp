258273d3185c65598b375b62b0054c22b9e1d86b
Sourav Kar <karsourav05@gmail.com>
2020-01-19 15:18:36 +0530

    sorted chk

--- Recursion & Backtracking/isSorted.cpp
+++ Recursion & Backtracking/isSorted.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void isSorted(int *a, int n) {
+bool isSorted(int *a, int n) {
 
 	//base case
-	if(n==0){
-		cout<<sorted;
-	}
+	if(n==1) return true;
 
 	//rec case
-	if(a[0]<)
+	if(a[0]<a[1] && isSorted(a+1,n-1)) return true;
+	return false;
 }
 
 int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
 
 	int n;
 	cin>>n;
@@ -20,7 +24,8 @@ int main() {
 	for(int i=0; i<n; i++)
 		cin>>a[i];
 
-	isSorted(a,n);
-	cout<<endl;
-	cout<<is_sorted(a);
+	if(isSorted(a,n)) cout<<"Sorted";
+	else cout<<"Not Sorted";
+
+	return 0;
 }
