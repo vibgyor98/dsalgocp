e4f692d46402f934e7eee0c0f79b3f6116a5d576
Sourav Kar <karsourav05@gmail.com>
2020-01-20 19:41:32 +0530

    isSorted Checked

--- Recursion & Backtracking/isSorted.cpp
+++ Recursion & Backtracking/isSorted.cpp
@@ -7,7 +7,7 @@ bool isSorted(int *a, int n) {
 	if(n==1) return true;
 
 	//rec case
-	if(a[0]<a[1] && isSorted(a+1,n-1)) return true;
+	if(a[0]<=a[1] && isSorted(a+1,n-1)) return true;
 	return false;
 }
 
