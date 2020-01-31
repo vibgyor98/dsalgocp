d454612e34ce289551ea8203673d80d13331c3fc
Sourav Kar <karsourav05@gmail.com>
2020-01-31 23:12:26 +0530

    kangarooStuckinBtwn

--- Hackerrank/kangaroo.cpp
+++ Hackerrank/kangaroo.cpp
@@ -10,8 +10,17 @@ int main() {
 
 	int x1,v1,x2,v2;
 	cin>>x1>>v1>>x2>>v2;
-	if((x1-x2)%(v2-v1)==0) cout<<"YES";
-	else cout<<"NO";
+
+	if(x2>x1 && v2>v1) cout<<"NO";
+	else {
+		for(int i=0; i<10000; i++) {
+			x1+=v1;
+			x2+=v2;
+			if(x1==x2) cout<<"YES";
+			break;
+		}
+		cout<<"NO";
+	}
 
 	return 0;
 }
