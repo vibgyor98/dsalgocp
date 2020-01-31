f10356d309f33cbabcaf1bb6e22d962ed7d3923f
Sourav Kar <karsourav05@gmail.com>
2020-01-31 21:05:15 +0530

    duplicatecharformatn

---
+++ Recursion & Backtracking/duplicate_char_formating.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void duplicate(char *s, int len) {
+	//base
+	if(l==0) {
+		return;
+	}
+	//rec
+	if
+
+}
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	char s[100];
+	cin>>s;
+	int l=s.length();
+	sort(s,s+l);
+	duplicate(s,l);
+
+
+	return 0;
+}
