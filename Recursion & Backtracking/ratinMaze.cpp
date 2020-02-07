0a1dd805657963fbef1e31d05998b3e53d8264cc
Sourav Kar <karsourav05@gmail.com>
2020-02-07 14:24:37 +0530

    Ratmze ist possible

--- Recursion & Backtracking/ratinMaze.cpp
+++ Recursion & Backtracking/ratinMaze.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 using namespace std;
 
-bool ratinmaze(int R, int C, char maze[][C], int sol[R][C], int i, int j, int m, int n) {
+bool isVisited[1001][1001], sol[1001][1001];
+bool ratinmaze(char maze[][1001], int i, int j, int m, int n) {
 	if(i==m && j==n) {
-		sol[m][n]=1;
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				cout<<sol[i][j]<<" ";
+		sol[i][j]=1;
+		for(int x=0; x<=m; x++) {
+			for(int y=0; y<=n; y++) {
+				cout<<sol[x][y]<<" ";
 			}
 			cout<<endl;
 		}
 		return true;
 	}
 
-	//Rat should be inside grid
-	if(i>m || j>n) {
-		return false;
-	}
-	if(maze[i][j]=='X') {
-		return false;
-	}
+	if(isVisited[i][j]) return false;
 
 	//Assume solution exsits through current cell
+	isVisited[i][j]=1;
 	sol[i][j]=1;
-	bool rhtSuccess = ratinmaze(R,C,maze,sol,i,j+1,m,n);
-	bool dwnSuccess = ratinmaze(R,C,maze,sol,i+1,j,m,n);
-	sol[i][j]=0;
-	if(rhtSuccess || dwnSuccess) {
-		return true;
+	if(j+1<=n && !isVisited[i][j+1]) {
+		bool rhtSuccess = ratinmaze(maze,i,j+1,m,n);
+		if(rhtSuccess) return true;
 	}
+	if(i+1<=m && !isVisited[i+1][j]) {
+		bool dwnSuccess = ratinmaze(maze,i+1,j,m,n);
+		if(dwnSuccess) return true;
+	}
+	sol[i][j]=0;
 	return false;
 }
 
@@ -39,21 +38,22 @@ int main() {
 	freopen("output.txt", "w", stdout);
 	#endif
 
-	int R,C;
-	cin>>R>>C;
-
 	//build the maze
-	char maze[R][C];
-	for(int i=0; i<R; i++) {
-		for(int j=0; j<C; j++) {
+	char maze[1001][1001];
+	int m,n;
+	cin>>m>>n;
+	for(int i=0; i<m; i++) {
+		for(int j=0; j<n; j++) {
 			cin>>maze[i][j];
+			if(maze[i][j]=='X') isVisited[i][j]=1;
 		}
 	}
+	m--;
+	n--;
 
-	int sol[R][C] = {0};
-	bool ans = ratinmaze(R,C,maze,sol,0,0,R-1,C-1);
+	int ans = ratinmaze(maze,0,0,m,n);
 	if(ans==false) {
-		cout<<"Path doesn't exist! ";
+		cout<<"-1";
 	}
 
 	return 0;
