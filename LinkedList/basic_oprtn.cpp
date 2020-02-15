ddd4fe314d1e1ab58edefaa454f6e597bb4c2784
Sourav Kar <karsourav05@gmail.com>
2020-02-15 19:11:33 +0530

    insert at middle stuck

--- LinkedList/basic_oprtn.cpp
+++ LinkedList/basic_oprtn.cpp
@@ -12,6 +12,16 @@ public:
 	}
 };
 
+//Calculate the length of the linked list
+int length(node*head) {
+	int len=0;
+	while(head!=NULL) {
+		head=head->next;
+		len+=1;
+	}
+	return len;
+}
+
 //Insert at Head
 void insertAtHead(node*&head, int data) {
 	node*n = new node(data);
@@ -19,14 +29,43 @@ void insertAtHead(node*&head, int data) {
 	head = n;
 }
 
+//Insert at tail
+void insertAtTail(node*&head, int data) {
+	if(head==NULL) {
+		head = new node(data);
+		return;
+	}
+	node*tail = head;
+	while(tail->next=NULL) {
+		tail=tail->next;
+	}
+	tail->next = new node(data);
+	return;
+}
+
 //Insert at Middle
-void insertAtMiddle(node*&head, int data) {
+void insertAtMiddle(node*&head, int data, int p) {
 	//chk if the current head is null
-	if(head->data==NULL) {
+	if(head->data==NULL||p==0) {
 		insertAtHead(head,data);
 	}
 	//chk if the node lies in tail
-	
+	else if(p>length(head)) {
+		insertAtTail(head,data);
+	}
+	//Actually inserting in the middle 
+	else {
+		//take p-1 jumps
+		int jump=1;
+		node*temp=head;
+		while(jump<=p-1) {
+			temp=temp->next;
+			jump+=1;
+		}
+		node*n = new node(data);
+		n->next=temp->next;
+		temp->next=n;
+	}
 }
 
 //print ll
@@ -51,8 +90,9 @@ int main() {
 	insertAtHead(head,5);
 	insertAtHead(head,4);
 	insertAtHead(head,3);
+	insertAtTail(head,6);
+	insertAtMiddle(head,2,3);
 
-	print(head);
 	print(head);
 
 	return 0;
