f2f5cfd3ff9ee3667f29f4875385e70e9942ab46
Sourav Kar <karsourav05@gmail.com>
2020-03-31 13:22:02 +0530

    pen paper sum

--- Arrays & String/Questions/Sum_Two_Arrays_Unique.cpp
+++ Arrays & String/Questions/Sum_Two_Arrays_Unique.cpp
@@ -3,21 +3,6 @@ using namespace std;
 
 void rough_add(int n, int *a1, int m, int *a2) {
 
-	if(n>m) {
-		for(int i=ist_last; i>=0; i--) {
-			sum = a1[i] + a2[i];
-			if(sum%10)
-		}
-	}
-
-	int ist_last = n-1;
-	int ist_last = m-1;
-	int ans[1001];
-	int sum = 0;
-	int carry = 0;
-	for(int i=1st_lst; i>=0; i--) {
-
-	}
 }
 
 int main() {
