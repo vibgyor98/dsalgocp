611b32496094dae6903b80e35129b285ea7cf146
Sourav Kar <karsourav05@gmail.com>
2020-04-02 19:06:54 +0530

    tricky

--- Codeforces/young_physicist.cpp
+++ Codeforces/young_physicist.cpp
@@ -10,23 +10,27 @@ int main() {
 
 	int n;
 	cin >> n;
-	int s1 = 0, s2 = 0, s3 = 0;
-	int x;
+	int a[n][n];
+	int s = 0;
 	for (int i = 0; i < n; i++) {
-		cin >> x;
-		s1 += x;
-	}
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		s2 += x;
-	}
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		s3 += x;
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
 	}
 
-	if ((s1 == 0 && s2 == 0) && s3 == 0) cout << "YES";
-	else cout << "NO";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			s += a[j][i];
+		}
+		cout << s << "\n";
+		if (s != 0) {
+			cout << "NO";
+			break;
+		}
+		s = 0;
+	}
+
+	if (s == 0) cout << "YES";
 
 	return 0;
 }
