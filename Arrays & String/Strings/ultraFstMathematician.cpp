ec29864d07061aab1b4ce0b4ca2ef699f8426f67
Sourav Kar <karsourav05@gmail.com>
2020-04-05 15:26:01 +0530

    prac

---
+++ Arrays & String/Strings/ultraFstMathematician.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    string s1, s2, s3 = "";
+    int i, j, t;
+    cin >> t;
+    while (t--) {
+        cin >> s1 >> s2;
+        for (i = 0; i < s1.length(); i++) {
+            if (s1[i] == s2[i]) s3.append("0");
+            else s3.append("1");
+        }
+        cout << s3 << endl;
+    }
+    return 0;
+}
+
+// input
+// 1
+// 10111 10000
+
+// output
+// 00111
