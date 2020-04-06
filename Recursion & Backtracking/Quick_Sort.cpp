77f663015fb88eb5e312bed3e1430f86a10ab5da
Sourav Kar <karsourav05@gmail.com>
2020-04-06 21:00:57 +0530

    prac

--- Recursion & Backtracking/Quick_Sort.cpp
+++ Recursion & Backtracking/Quick_Sort.cpp
@@ -6,58 +6,58 @@ using namespace std;
 //shuffle the idx
 void shuffle(int *a, int s, int e) {
 	srand(time(NULL));
-	for(int i=e; i>0; i--) {
-		int j=rand()%(i+1);
-		swap(a[i],a[j]);
+	for (int i = e; i > 0; i--) {
+		int j = rand() % (i + 1);
+		swap(a[i], a[j]);
 	}
 }
 
 int partition(int *a, int s, int e) {
-	int pivot=a[e];
-	int i=s-1;
-	int j=s;
-	for(;j<e;j++) {
-		if(a[j]<=pivot) {
+	int pivot = a[e];
+	int i = s - 1;
+	int j = s;
+	for (; j < e; j++) {
+		if (a[j] <= pivot) {
 			i++;
-			swap(a[i],a[j]);
+			swap(a[i], a[j]);
 		}
 	}
 	//bring the pivot ele to its sorted pos
-	swap(a[i+1],a[e]);
+	swap(a[i + 1], a[e]);
 	//idx of the pivot ele
-	return i+1;
+	return i + 1;
 }
 
 void quickSort(int *a, int s, int e) {
 	//base
-	if(s>=e) {
+	if (s >= e) {
 		return;
 	}
 	//rec
-	int pivot = partition(a,s,e);
-	quickSort(a,s,pivot-1);
-	quickSort(a,pivot+1,e);
+	int pivot = partition(a, s, e);
+	quickSort(a, s, pivot - 1);
+	quickSort(a, pivot + 1, e);
 }
 
 int main() {
 
-	#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	#endif
+#endif
 
 	int n;
-	cin>>n;
+	cin >> n;
 	int a[n];
-	for(int i=0; i<n; i++) {
-		cin>>a[i];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
 	}
 
-	shuffle(a,0,n-1);
-	quickSort(a,0,n-1);
+	shuffle(a, 0, n - 1); //randomized
+	quickSort(a, 0, n - 1);
 
-	for(int i=0; i<n; i++) {
-		cout<<a[i]<<" ";
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
 	}
 
 	return 0;
--- Sorting & Searching/Count_Sort.cpp
+++ Sorting & Searching/Count_Sort.cpp
@@ -1,45 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-#define ll long long int
-#define fix INT_MAX
+void countSort(vector <int>& arr) {
+	int max = *max_element(arr.begin(), arr.end());
+	int min = *min_element(arr.begin(), arr.end());
+	int range = max - min + 1;
 
+	vector<int> count(range), output(arr.size());
+	for (int i = 0; i < arr.size(); i++) count[arr[i] - min]++;
+
+	for (int i = 1; i < count.size(); i++) count[i] += count[i - 1];
+
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		output[ count[arr[i] - min] - 1 ] = arr[i];
+		count[arr[i] - min]--;
+	}
+
+	for (int i = 0; i < arr.size(); i++) arr[i] = output[i];
+}
+
+void printArray(vector <int> & arr) {
+	for (int i = 0; i < arr.size(); i++) cout << arr[i] << " ";
+	cout << "\n";
+}
 
 int main() {
 
-	#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	#endif
+#endif
 
-	ll n;
-	cin>>n;
-	ll a[n];
-	ll freq[fix] = {};
-	for(ll i=0; i<n; i++) {
-		cin>>a[i];
-		fix[a[i]]+=1;
+	int n;
+	cin >> n;
+	vector<int> arr;
+	int x;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		arr.push_back(x);
 	}
-
-	
-	for(int i=0; i<n; i++) {
-		if(fix[i]==0)
-			continue;
-		else {
-			if(fix[i]>0) {
-				for(int j=fix[i]; i>0; i--) {
-					cout<<fix[i]<<" ";
-				}
-			}
-		}
-	}
-
+	countSort (arr);
+	printArray (arr);
 	return 0;
 }
 
-//Input
-// 8
-// 1 3 4 5 5 2 7 8
 
-//Output
-// 2 1 4 3 6 5
+// input
+// 5
+// 1 3 2 6 5
+
+// output
+// 1 2 3 5 6
