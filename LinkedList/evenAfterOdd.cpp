4244ff1de9fa87bc54ed2c08b521e3bd5bddd231
vibgyor98 <www.sunny02@gmail.com>
2020-11-05 12:56:02 +0530

    ++ prac

--- LinkedList/evenAfterOdd.cpp
+++ LinkedList/evenAfterOdd.cpp
@@ -1,42 +1,74 @@
 #include <iostream>
 using namespace std;
 
-class node {
+class Node {
 public:
 	int data;
-	node* next;
+	Node* next;
 
-	node(int d) {
+	Node(int d) {
 		data=d;
 		next=NULL;
 	}
 };
 
-void insertAtTail(node*&head, int data) {
-	if(head==NULL) {
-		head = new node(data);
+void separateOddAndEven(Node** head_ref) {
+	Node* end = *head_ref;
+	Node* prev = NULL;
+	Node* curr = *head_ref;
+
+	while(end->next != NULL) end = end->next;
+
+	Node* new_end = end;
+
+	while(curr->data % 2 == 0 && curr != end) {
+		new_end->next = curr;
+		curr = curr->next;
+		new_end->next = NULL;
+		new_end = new_end->next;
+	}
+
+	if(curr->data % 2 != 0) {
+		*head_ref = curr;
+		while(curr != end) {
+			if((curr->data) % 2 != 0) {
+				prev = curr;
+				curr = curr->next;
+			} else {
+				prev->next = curr->next;
+				curr->next = NULL;
+				new_end->next = curr;
+				new_end = curr;
+				curr = prev->next;
+			}
+		}
+	} else {
+		prev = curr;
+	}
+	if(new_end != end && (end->data) % 2 == 0) {
+		prev->next = end->next;
+		end->next = NULL;
+		new_end->next = end;
+	}
+	return;
+} 
+
+void insertAtTail(Node* &head, int data) {
+	if(head == NULL) {
+		head = new Node(data);
 		return;
 	}
-	node*tail = head;
-	while(tail->next!=NULL) tail=tail->next;
-	tail->next = new node(data);
+	Node* tail = head;
+	while(tail->next != NULL) tail = tail->next;
+	tail->next = new Node(data);
 	return;
 }
 
-void buildList(node*&head, int n) {
-	while(n--) {
-		int data;
-		cin>>data;
-		insertAtTail(head,data);
+void printList(Node* head) {
+	while(head != NULL) {
+		cout << head->data <<" ";
+		head = head->next;
 	}
-}
-
-void printList(node*head) {
-	while(head!=NULL) {
-		cout<<head->data<<"-->";
-		head=head->next;
-	}
-	cout<<endl;
 }
 
 int main() {
@@ -45,13 +77,21 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	#endif
+	ios_base::sync_with_stdio(false);	
+	cin.tie(NULL); cout.tie(NULL);
 
-	int n;
-	cin>>n;
-	node*head=NULL;
-	buildList(head,n);
+	Node* head = NULL;
+	int n; cin >> n;
+	int x;
+	for(int i = 0; i < n; i++) {
+		cin >> x;
+		insertAtTail(head,x);
+	}
 
+	//passing value by ref
+	separateOddAndEven(&head);
 	printList(head);
+
 	return 0;
 }
 
--- LinkedList/kthEle_fromLst.cpp
+++ LinkedList/kthEle_fromLst.cpp
@@ -51,6 +51,8 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	#endif
+	ios_base::sync_with_stdio(false);	
+	cin.tie(NULL); cout.tie(NULL);
 
 	node*head = NULL;
 	buildList(head);
