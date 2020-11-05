4085d18fd4db228ba944855291f3e721e7f95173
vibgyor98 <www.sunny02@gmail.com>
2020-11-05 13:26:50 +0530

    len + n - 1

--- LinkedList/kthEle_fromLst.cpp
+++ LinkedList/kthEle_fromLst.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class node {
+class Node {
 public:
 	int data;
-	node* next;
+	Node* next;
 
-	node(int d) {
+	Node(int d) {
 		data=d;
 		next=NULL;
 	}
 };
 
-//Insert at tail
-void insertAtTail(node*&head, int data) {
-	if(head==NULL) {
-		head = new node(data);
+void insertAtTail(Node* &head, int data) {
+	if(head == NULL) {
+		head = new Node(data);
 		return;
 	}
-	node*tail = head;
-	while(tail->next!=NULL) {
-		tail=tail->next;
-	}
-	tail->next = new node(data);
+	Node* tail = head;
+	while(tail->next != NULL) tail = tail->next;
+	tail->next = new Node(data);
 	return;
 }
 
-//building List
-void buildList(node*&head) {
-	int data;
-	cin>>data;
-	while(data!=-1) {
-		insertAtTail(head,data);
-		cin>>data;
+void solve(Node* &head, int k) {
+	int len = 0, i;
+	Node* temp = head;
+	while(temp != NULL) {
+		temp = temp->next;
+		len++;
 	}
-}
-
-//print
-void printList(node*head) {
-	while(head!=NULL) {
-		cout<<head->data<<"-->";
-		head=head->next;
-	}
-	cout<<endl;
+	if(len < k) return;
+	temp = head;
+	for(i = 1; i < len - k + 1; i++) temp = temp->next;
+	cout << temp->data;
 }
 
 int main() {
@@ -54,9 +45,31 @@ int main() {
 	ios_base::sync_with_stdio(false);	
 	cin.tie(NULL); cout.tie(NULL);
 
-	node*head = NULL;
-	buildList(head);
-	printList(head);
+	Node* head = NULL;
+	int data, k;
+	while(data != -1) {
+		cin >> data;
+		if(data != -1) insertAtTail(head,data);
+	}
+	cin >> k;
+	solve(head, k);
 
 	return 0;
 }
+
+/*
+Constraints
+n < 10^5
+
+Output Format
+Output a single line containing the node value at the kth element from last.
+
+Sample Input
+1 2 3 4 5 6 -1
+3
+Sample Output
+4
+Explanation
+The linked list is 1 2 3 4 5 6. -1 is not included in the list.
+So the third element from the last is 4
+*/
